94324d41d1bbdb68bb3549c405fc158cce247834
orIfrach <ore246@gmail.com>
2023-09-10 10:42:55 +0300

    modified:   src/components/LolMain.jsx

--- src/components/LolMain.jsx
+++ src/components/LolMain.jsx
@@ -52,7 +52,7 @@ function LolMain() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentChampionIndex((prevIndex) => (prevIndex + 1) % championData.length);
-    }, 10000); 
+    }, 4000); 
 
     return () => clearInterval(interval);
   }, []);
