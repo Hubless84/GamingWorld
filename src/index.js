3412516579ca101f0834a65fbe8b95a05882313c
orIfrach <ore246@gmail.com>
2023-09-10 14:49:31 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-        <App />
-, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
